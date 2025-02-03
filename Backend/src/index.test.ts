c82185fc175ab15626d0474bf1ce43e10fbff81c
jhotchk2 <jhotchk2@emich.edu>
2025-02-03 18:40:31 -0500

    updated jest test in backend to finish running

--- Backend/src/index.test.ts
+++ Backend/src/index.test.ts
@@ -1,10 +1,23 @@
 import request from 'supertest'
-// import express from 'express'
-import app from './index'
+import express from 'express'
+
+const app = express()
+let server
+
+beforeAll(() => {
+  app.get('/jest', (req, res) => {
+    res.send('hello')
+  })
+  server = app.listen(8000)
+})
+
+afterAll(() => {
+  server.close()
+})
 
 describe('Root Route', function () {
   test('Home route should work', async () => {
-    const res = await request(app).get('/')
+    const res = await request(app).get('/jest')
     expect(res.text).toEqual('hello')
   })
 })
