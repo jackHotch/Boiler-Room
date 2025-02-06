f9960c83a37f755e1d21224fafa03d9cf40270cd
jhotchk2 <jhotchk2@emich.edu>
2025-02-06 15:57:49 -0500

    added css to navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,33 @@
+'use client'
+
 import styles from './Navbar.module.css'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export function Navbar() {
+  const pathname = usePathname()
+
+  if (pathname == '/') return null
+
   return (
     <div className={styles.container}>
-      <Link href='/'>Boiler Room</Link>
+      <Link className={styles.logo} href='/'>
+        Boiler Room
+      </Link>
       <div className={styles.nav_options}>
-        <Link href='/Friends'>Friends</Link>
-        <Link href='/NewGameRecs'>New Game Recomendations</Link>
-        <input type='text' placeholder='Search' />
-        <Link href='/Accounts'>Account</Link>
+        <Link className={styles.links} href='/Friends'>
+          Friends
+        </Link>
+        <Link className={styles.links} href='/NewGameRecs'>
+          New Game Recomendations
+        </Link>
+        <div className={styles.searchbar}>
+          <input className={styles.input} type='text' placeholder='Search' />
+          <img src='/search.png' className={styles.search_icon} width={16} />
+        </div>
+        <Link className={`${styles.links} ${styles.account}`} href='/Accounts'>
+          P
+        </Link>
       </div>
     </div>
   )
