5fa0a0f743c71a81da0cce12a547f2f60ba1ba85
Boonem <mcboone228@gmail.com>
2025-02-10 13:47:12 -0500

    Created dashboard page file

---
+++ frontend/src/app/Dashboard/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Dashboard.module.css';
+
+export default function Dashboard() {
+    return (
+        <h1 className={styles.header}>Dashboard</h1>
+    );
+
+}
