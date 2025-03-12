4f9a2eac44d976d777cbe701d4bd34457a681ddd
Christopher Holman <cholman5@emich.edu>
2025-03-12 14:12:47 -0400

    Fixed malformed SQL queries

--- Backend/src/index.test.ts
+++ Backend/src/index.test.ts
@@ -12,7 +12,7 @@ const pool = new Pool({
 
 beforeAll(async () => {
   await pool.connect();
-  await pool.query('DELETE FROM Profile WHERE steam_id = $1', [testSteamId.toString()]);
+  await pool.query('DELETE FROM "Profile" WHERE steam_id = $1', [testSteamId.toString()]);
 });
 
 afterAll(async () => {
@@ -28,7 +28,7 @@ test('insertProfile inserts a profile and returns true', async () => {
   const result = await insertProfile(BigInt(testSteamId));
   expect(result).toBe(true);
 
-  const res = await pool.query('SELECT * FROM Profile WHERE steam_id = $1', [testSteamId.toString()]);
+  const res = await pool.query('SELECT * FROM "Profile" WHERE steam_id = $1', [testSteamId.toString()]);
   expect(res.rows[0].steam_id).toEqual(testSteamId.toString());
   expect(res.rows[0].username).toEqual(testProfile.username);
   expect(res.rows[0].avatar_hash).toEqual(testProfile.avatar_hash);
@@ -41,7 +41,7 @@ test('insertGames inserts games and returns success message', async () => {
   const result = await insertGames(BigInt(testSteamId));
   expect(result).toEqual({ success: true, message: 'Games inserted/updated successfully.' });
 
-  const res = await pool.query('SELECT * FROM User_Games WHERE steam_id = $1', [testSteamId.toString()]);
+  const res = await pool.query('SELECT * FROM "User_Games" WHERE steam_id = $1', [testSteamId.toString()]);
   expect(res.rows[0].steam_id).toEqual(testSteamId.toString());
   expect(res.rows[0].game_id).toEqual(testUserGames.game_id);
   expect(res.rows[0].total_played).toEqual(testUserGames.total_played);
