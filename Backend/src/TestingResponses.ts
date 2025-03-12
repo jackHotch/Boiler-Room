f299c17d8a87d1a9ad5b903910b9b0b9e355f338
Christopher Holman <cholman5@emich.edu>
2025-03-12 17:47:06 -0400

    Merge my tests with jacks

--- Backend/src/TestingResponses.ts
+++ Backend/src/TestingResponses.ts
@@ -1,29 +1,80 @@
-//export const testSteamId = '76561197960434622'
-export const testSteamId = BigInt(76561199154033472n);
+export const testFriendsList = [
+  {
+      "steamid": "76561197960265731",
+      "relationship": "friend",
+      "friend_since": 0
+  },
+  {
+      "steamid": "76561197960265738",
+      "relationship": "friend",
+      "friend_since": 0
+  },
+  {
+      "steamid": "76561197960265740",
+      "relationship": "friend",
+      "friend_since": 0
+  },
+  {
+      "steamid": "76561197960265744",
+      "relationship": "friend",
+      "friend_since": 1585508613
+  },
+  {
+      "steamid": "76561197960265747",
+      "relationship": "friend",
+      "friend_since": 0
+  }
+]
+
+export const testRecentGames = [
+  {
+      "appid": 2669320,
+      "name": "EA SPORTS FC 25",
+      "playtime_2weeks": 510,
+      "playtime_forever": 510,
+      "img_icon_url": "f6c4988f3e7b965451a3d9fca34014585d1754e2",
+      "playtime_windows_forever": 510,
+      "playtime_mac_forever": 0,
+      "playtime_linux_forever": 0,
+      "playtime_deck_forever": 0
+  },
+  {
+      "appid": 2108330,
+      "name": "F1® 23",
+      "playtime_2weeks": 32,
+      "playtime_forever": 3090,
+      "img_icon_url": "71ea543e1ea5d6898ec2012e48db08c6401b5365",
+      "playtime_windows_forever": 3090,
+      "playtime_mac_forever": 0,
+      "playtime_linux_forever": 0,
+      "playtime_deck_forever": 0
+  },
+  {
+      "appid": 3097560,
+      "name": "Liar's Bar",
+      "playtime_2weeks": 1,
+      "playtime_forever": 264,
+      "img_icon_url": "5342338ad1fd3561c1ed8c6d2ed4ae0e6ada2e9c",
+      "playtime_windows_forever": 264,
+      "playtime_mac_forever": 0,
+      "playtime_linux_forever": 0,
+      "playtime_deck_forever": 0
+  }
+]
+
+export const testPlayerSummary = {
+  "username": "Robin",
+  "userImage": "https://avatars.steamstatic.com/81b5478529dce13bf24b55ac42c1af7058aaf7a9_full.jpg"
+}
 
 export const testGameDetails = {
   "name": "Counter-Strike",
   "header_image": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/10/header.jpg?t=1729702322",
   "description": "Play the world's number 1 online action game. Engage in an incredibly realistic brand of terrorist warfare in this wildly popular team-based game. Ally with teammates to complete strategic missions. Take out enemy sites. Rescue hostages. Your role affects your team's success. Your team's success affects your role.",
-  "hltb_score": 66.3,
+  "hltb_score": null,
   "recommendations": "depreciated",
   "price": 10,
   "metacritic_score": 88,
   "released": "2000-11-01T05:00:00.000Z",
   "platform": 7
-}
-
-export const testUserGames = {
-  "game_id": 10,
-  "steam_id": "76561199154033472",
-  "total_played": 0,
-  "last_2_weeks": null,
-  "recency": 0
-}
-
-export const testProfile = {
-  "steam_id": "76561199154033472",
-  "username": "thorn1000",
-  "avatar_hash": "5538e220506aea324c1adfb5c53587625b2c939e",
-  "preference": 0
 }
