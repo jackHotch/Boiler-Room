354a0e51ee723f84bb5bbb394d9fade9ed7be01b
jhotchk2 <jhotchk2@emich.edu>
2025-03-18 16:08:22 -0400

    another update for puppeteer config file

--- Backend/puppeteer.config.cjs
+++ Backend/puppeteer.config.cjs
@@ -1,7 +1,12 @@
-if (process.env.NODE_ENV == 'production') {
-  const { join } = require('path');
-  /**
-  * @type {import("puppeteer").Configuration}
-  */
-  module.exports = {cacheDirectory: join(__dirname, '.cache', 'puppeteer')};
-}
+const { join } = require('path');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  cacheDirectory: isProduction
+    ? join(__dirname, '.cache', 'puppeteer')  // Production path
+    : undefined // Default for local, letting Puppeteer manage it
+};
