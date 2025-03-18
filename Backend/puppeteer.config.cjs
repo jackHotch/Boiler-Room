564afce8c602783ee3e0d9c3c1771cd503add649
jhotchk2 <jhotchk2@emich.edu>
2025-03-18 15:58:16 -0400

    updated puppeteer config file

--- Backend/puppeteer.config.cjs
+++ Backend/puppeteer.config.cjs
@@ -1,5 +1,7 @@
-const { join } = require('path');
-/**
-* @type {import("puppeteer").Configuration}
-*/
-module.exports = {cacheDirectory: join(__dirname, '.cache', 'puppeteer')};
+if (process.env.NODE_ENV == 'production') {
+  const { join } = require('path');
+  /**
+  * @type {import("puppeteer").Configuration}
+  */
+  module.exports = {cacheDirectory: join(__dirname, '.cache', 'puppeteer')};
+}
