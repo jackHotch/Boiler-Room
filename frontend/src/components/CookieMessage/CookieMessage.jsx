fffa5638dc891e14b076bea774ad57de8349d276
jhotchk2 <jhotchk2@emich.edu>
2025-03-24 14:47:59 -0400

    created message for landing page

---
+++ frontend/src/components/CookieMessage/CookieMessage.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import styles from './CookieMessage.module.css'
+
+export function CookieMessage() {
+  const [showToast, setShowToast] = useState(false)
+
+  useEffect(() => {
+    const showTimer = setTimeout(() => {
+      setShowToast(true)
+    }, 3000)
+
+    const hideTimer = setTimeout(() => {
+      setShowToast(false)
+    }, 6000)
+
+    return () => {
+      clearTimeout(showTimer)
+      clearTimeout(hideTimer)
+    }
+  })
+
+  if (showToast == false) {
+    return null
+  }
+
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.heading}>!!Firefox Users!!</h3>
+      <p className={styles.message}>
+        Please enables cookies in order to continue to use our website
+      </p>
+    </div>
+  )
+}
