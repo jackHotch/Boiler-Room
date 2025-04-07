68c43c4a22b1f9adcc6aa5336668bd744ff764ab
jhotchk2 <jhotchk2@emich.edu>
2025-04-07 17:34:00 -0400

    fixed issue with searchbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -19,24 +19,12 @@ export function Navbar() {
   const [showSteam, setShowSteam] = useState(false) // Track hover
   const [steamPFP, setPFP] = useState(null)
   const [loading, setLoading] = useState(true)
-  const [theme, setTheme] = useState(true)
+  const [theme] = useState(true)
 
   if (pathname === '/' || pathname === '/LoginRedirect') return null
 
   useEffect(() => {
     async function fetchProfileData() {
-      try {
-        const theme = await axios.get(
-          process.env.NEXT_PUBLIC_BACKEND + '/themepreference',
-          {
-            withCredentials: true,
-          }
-        )
-      } catch (err) {
-        console.error(err)
-      }
-      setTheme(theme)
-
       try {
         const response = await axios.get(
           `${process.env.NEXT_PUBLIC_BACKEND}/steam/getdisplayinfo`,
