03cf12cd7053949c9ba71fd3dfb2360870ec407e
Trevor Small <tsmall4@emich.edu>
2025-04-10 00:19:48 -0400

    Added missing tooltip icon to BoilRating on the SingleGamePage

--- frontend/src/app/SingleGame/[gameid]/page.jsx
+++ frontend/src/app/SingleGame/[gameid]/page.jsx
@@ -184,6 +184,8 @@ const SingleGamePage = () => {
 
           <div className={styles.boil}>
             <strong>Boil Rating</strong>
+            <strong className={styles.questionMark}> ?</strong>
+
             <br />
             <br />
             <span className={styles.boil_score}>
